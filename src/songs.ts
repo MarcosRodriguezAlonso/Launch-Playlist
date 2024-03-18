4aecbb9717b92bcb900f9a6002d7616ad023cae4
Marcos Rodríguez <e.marcosra@gmail.com>
2024-03-18 14:26:36 +0100

    add requirements for delivery

--- src/songs.ts
+++ src/songs.ts
@@ -1,9 +1,11 @@
+import { title } from "process";
+
 export const isTitleEmpty = (songTitle: string): boolean => {
   let isEmpty: boolean;
   // Asígnale a la variable isEmpty un true o un false dependiendo de si
   // la variable songTitle recibida tiene un string vacío o no
 
-  isEmpty = true; // Cambia esta línea por tu código
+  isEmpty = songTitle.length === 0;
 
   return isEmpty;
 };
@@ -16,7 +18,7 @@ export const doesTitleExist = (
   // Asígnale a la variable doesExist un true o un false dependiendo de si
   // la variable title recibida ya existe en el array de títulos recibido
 
-  doesExist = true; // Cambia esta línea por tu código
+  doesExist = title.includes(songTitle);
 
   return doesExist;
 };
@@ -26,7 +28,7 @@ export const isTitleShort = (songTitle: string): boolean => {
   // Asígnale a la variable isShort un true o un false dependiendo de si
   // la variable title recibida tiene menos de 3 caracteres
 
-  isShort = true; // Cambia esta línea por tu código
+  isShort = title.length < 3;
 
   return isShort;
 };
@@ -36,25 +38,26 @@ export const isPlaylistFull = (songTitles: string[]): boolean => {
   // Asígnale a la variable isFull un true o un false dependiendo de si
   // el array de títulos recibido tiene 5 o más elementos
 
-  isFull = true; // Cambia esta línea por tu código
+  isFull = songTitles.length >= 5;
 
   return isFull;
 };
 
 export const addSong = (songTitle: string, songTitles: string[]): void => {
   // Añade la variable title al array de títulos recibido
+  songTitles.push(title);
 };
 
 export const sortSongs = (songTitles: string[]): void => {
   // Ordena alfabéticamente el array de títulos recibido
+  songTitles.sort();
 };
 
 export const getSongsCount = (songTitles: string[]): number => {
   let songsCount: number;
   // Asígnale a la variable songsCount el número de elementos del array recibido
 
-  songsCount = 0; // Cambia esta línea por tu código
-
+  songsCount = songTitles.length;
   return songsCount;
 };
 
@@ -63,21 +66,28 @@ export const removeSongByPosition = (
   position: number
 ): void => {
   // Elimina del array de títulos recibido el elemento que se encuentra en la posición recibida
+  songTitles.splice(position);
 };
 
 export const getErrorMessage = (errorCode: string): string => {
   let errorMessage: string;
 
-  // A la variable anterior le tendrás que asignar un mensaje de error
-  // dependiendo del código de error recibido
-  // Si el código de error no es válido, asígnale un mensaje genérico
-  // Éstos son los códigos de error y sus mensajes correspondientes:
-  // - "required": "No has introducido ningún título"
-  // - "exists": "La canción ya existe"
-  // - "too-short": "El título es demasiado corto"
-  // - "limit": "La playlist está llena"
+  switch (errorCode) {
+    case "required":
+      errorMessage = "No has introducido ningún título";
+      break;
+    case "exists":
+      errorMessage = "La canción ya existe";
+      break;
+    case "too-short":
+      errorMessage = "El título es demasiado corto";
+      break;
+    case "limit":
+      errorMessage = "La playlist está llena";
+      break;
+    default:
+      errorMessage = "Error, algo no ha ido bien";
 
-  errorMessage = ""; // Cambia esta línea por tu código
-
-  return errorMessage;
+      return errorMessage;
+  }
 };
